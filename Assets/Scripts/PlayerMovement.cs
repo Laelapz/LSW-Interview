70631aa8bfc9ef34262a6b563513905537c221f3
Johnny <johnnyf.birck@gmail.com>
2022-08-22 01:45:45 -0300

    Created base for animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,20 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] float _Speed = 3;
 
+
+
     InputActions _Input;
     Rigidbody2D _Rigidbody;
+    SpriteRenderer _SpriteRenderer;
+    Animator _Animator;
     Vector2 _Movement;
 
     private void Awake()
     {
         _Input = new InputActions();
         _Rigidbody = GetComponent<Rigidbody2D>();
+        _SpriteRenderer = GetComponent<SpriteRenderer>();
+        _Animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -34,6 +40,9 @@ public class PlayerMovement : MonoBehaviour
     private void OnMovement(InputAction.CallbackContext ctx)
     {
         _Movement = ctx.ReadValue<Vector2>();
+        _Animator.SetFloat("Horizontal", _Movement.x);
+        _Animator.SetFloat("Vertical", _Movement.y);
+        _Animator.SetFloat("Speed", _Movement.sqrMagnitude);
     }
 
     private void Update()
